1eae8987e6aa37d9bfeba798a1ad60bfd57968b7
hammsik <asbbrp@gmail.com>
2024-03-16 19:10:22 +0900

    feat: 웹뷰 테스트

--- frontend/project_flutter/lib/allbome.dart
+++ frontend/project_flutter/lib/allbome.dart
@@ -5,76 +5,68 @@ import 'package:project_flutter/layout_nav3.dart';
 import 'package:project_flutter/layout_nav4.dart';
 import 'package:project_flutter/layout_nav5.dart';
 
-class AllBome extends StatelessWidget {
+class AllBome extends StatefulWidget {
   const AllBome({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const NavigationExample(),
-    );
-  }
+  State<AllBome> createState() => _AllBomeState();
 }
 
-class NavigationExample extends StatefulWidget {
-  const NavigationExample({super.key});
-
-  @override
-  State<NavigationExample> createState() => _NavigationExampleState();
-}
-
-class _NavigationExampleState extends State<NavigationExample> {
+class _AllBomeState extends State<AllBome> {
   int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: const Color(0xFF379237),
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: '1',
+    // final ThemeData theme = Theme.of(context);
+    return MaterialApp(
+      home: Scaffold(
+          bottomNavigationBar: NavigationBar(
+            onDestinationSelected: (int index) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+            // elevation: 0.5,
+            // shadowColor: Colors.black,
+            labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+            // indicatorColor: const Color(0xFF379237),
+            selectedIndex: currentPageIndex,
+            destinations: const <Widget>[
+              NavigationDestination(
+                // selectedIcon: Icon(Icons.sports_esports_rounded),
+                icon: Icon(Icons.sports_esports_rounded),
+                label: '1',
+              ),
+              NavigationDestination(
+                // selectedIcon: Icon(Icons.work),
+                icon: Icon(Icons.work),
+                label: '2',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.local_florist_rounded),
+                label: '3',
+              ),
+              NavigationDestination(
+                icon: Badge(
+                  child: Icon(Icons.alarm_rounded),
+                ),
+                label: '4',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.map_outlined),
+                label: '5',
+              ),
+            ],
           ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: '2',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: '3',
-          ),
-          NavigationDestination(
-            icon: Badge(child: Icon(Icons.notifications_sharp)),
-            label: '4',
-          ),
-          NavigationDestination(
-            icon: Badge(
-              label: Text('2'),
-              child: Icon(Icons.messenger_sharp),
-            ),
-            label: '5',
-          ),
-        ],
-      ),
-      body: <Widget>[
-        LayoutNav1(),
-        LayoutNav2(),
-        LayoutNav3(),
-        LayoutNav4(),
-        LayoutNav5(),
-      ][currentPageIndex],
+          body: SafeArea(
+            child: <Widget>[
+              LayoutNav1(),
+              LayoutNav2(),
+              LayoutNav3(),
+              LayoutNav4(),
+              LayoutNav5(),
+            ][currentPageIndex],
+          )),
     );
   }
 }
--- frontend/project_flutter/lib/layout_nav3.dart
+++ frontend/project_flutter/lib/layout_nav3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class LayoutNav3 extends StatefulWidget {
   const LayoutNav3({super.key});
@@ -10,12 +11,37 @@ class LayoutNav3 extends StatefulWidget {
 }
 
 class _LayoutNav3State extends State<LayoutNav3> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://student.readforschool.com/'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('3'),
-      ),
+      child: Center(child: WebViewWidget(controller: _controller)),
     );
   }
 }
