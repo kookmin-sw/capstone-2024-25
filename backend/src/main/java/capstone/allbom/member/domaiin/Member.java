451b290685a718871bf6813b6e038db3cf2330b0
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-19 19:39:02 +0900

    feat: Member 엔티티 설계

---
+++ backend/src/main/java/capstone/allbom/member/domaiin/Member.java
@@ -0,0 +1,54 @@
+package capstone.allbom.member.domaiin;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String kakaoEmail;
+
+    private String login_id;
+
+    private String login_password;
+
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+
+    private gender gender;
+
+    @Embedded
+    private Address address;
+
+    private Double longitude;
+
+    private Double latitude;
+
+    private String phoneNumber;
+
+    private String guardianNumber;
+
+    private String profileImageUrl;
+
+
+
+
+
+
+
+}
