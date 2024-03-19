9524c8106f12cb2b87db6ebc669e20b3fc007578
Vargun <heegun4690@naver.com>
2024-03-19 17:24:42 +0900

    feat: Sign In - 카카오 로그인 개발

--- frontend/project_react/src/pages/SignIn.js
+++ frontend/project_react/src/pages/SignIn.js
@@ -10,7 +10,6 @@ const SignUpWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  border: 1px solid blue;
   padding: 60px 28px;
   box-sizing: border-box;
 `;
@@ -22,7 +21,6 @@ const SignUpContent = styled.div`
   align-items: center;
   gap: 32px;
   width: 100%;
-  border: 1px solid red;
 `;
 
 const FooterText = styled.div`
@@ -40,8 +38,37 @@ const HorizontalLine = styled.div`
   height: 1px;
   background-color: var(--unselected-color);
 `;
+const KakaoButton = styled.div`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 36px;
+  background-color: #fee502;
+  border: none;
+  color: #000000;
+  border-radius: 10px;
+  font-size: 24px;
+  position: relative;
+`;
+
+const KakaoImage = styled.img`
+  position: absolute;
+  left: 16px;
+  width: 28px;
+  height: 28px;
+`;
 
 const SignUp = () => {
+  const Rest_api_key = process.env.REACT_APP_REST_API_KEY; //REST API KEY
+  const redirect_uri = 'http://localhost:3000/api/user/kakao/callback'; //Redirect URI
+  // oauth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
+
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   return (
@@ -70,6 +97,13 @@ const SignUp = () => {
         </FooterText>
         <HorizontalLine />
         <FooterText>다른 방법으로 로그인하기</FooterText>
+        <KakaoButton onClick={handleLogin}>
+          <KakaoImage
+            src={process.env.PUBLIC_URL + '/images/kakao.svg'}
+            alt="kakao"
+          />
+          카카오 로그인
+        </KakaoButton>
       </SignUpContent>
     </SignUpWrapper>
   );
