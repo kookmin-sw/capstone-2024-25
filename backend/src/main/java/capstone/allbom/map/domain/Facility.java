1f73561dc246fdcca52f4452b3fb1afd1d5632d1
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-20 22:00:15 +0900

    feat: Facility 엔티티(병원, 약국, 복지주택, 복지관, 케어센터 포함) 설계

---
+++ backend/src/main/java/capstone/allbom/map/domain/Facility.java
@@ -0,0 +1,23 @@
+package capstone.allbom.map.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Facility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "facility_id")
+    private Long id;
+
+    private String name;
+
+    private FacilityType type;
+
+    private Double latitude;
+
+    private Double longitude;
+}
