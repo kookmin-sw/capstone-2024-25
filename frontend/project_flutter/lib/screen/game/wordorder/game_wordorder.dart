a6bdf1b49e6c42e663a5ce6e727ddf72c5662608
hammsik <asbbrp@gmail.com>
2024-03-21 02:29:52 +0900

    Feat: 두번째 페이지 테스트 및 헤더 변경

---
+++ frontend/project_flutter/lib/screen/game/wordorder/game_wordorder.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+
+// class WordOrderGame extends StatefulWidget {
+//   const WordOrderGame({super.key});
+
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     return _WordOrderGameState();
+//   }
+// }
+
+// class _WordOrderGameState() extends State<WordOrderGame> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(children: ["Word Order Game", "This is a word order game."]);
+//   }
+//   }
--- frontend/project_flutter/lib/widget/header.dart
+++ frontend/project_flutter/lib/widget/header.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 
-Widget HeaderWidget(
-    {String? title, Alignment? itemAlignment, bool? isShowBackButton}) {
-  return Container(
-    padding: const EdgeInsets.all(20),
-    alignment: itemAlignment,
-    child: Row(
-      children: [
-        if (isShowBackButton == true)
-          IconButton(
+// Widget HeaderWidget(
+//     {String? title, Alignment? itemAlignment, bool? isShowBackButton}) {
+//   return Container(
+//     padding: const EdgeInsets.all(20),
+//     alignment: itemAlignment,
+//     child: Row(
+//       children: [
+//         if (isShowBackButton == true)
+//           IconButton(
+//             icon: const Icon(Icons.arrow_back_ios),
+//             onPressed: () {
+//               print('Back button is clicked');
+//             },
+//           ),
+//         if (title != null)
+//           Text(
+//             title,
+//             style: const TextStyle(
+//               fontSize: 20,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//       ],
+//     ),
+//   );
+// }
+
+Widget HeaderWidget({String? title, bool? isShowBackButton}) {
+  return AppBar(
+    centerTitle: isShowBackButton,
+    title: Text(title ?? ''),
+    leading: isShowBackButton == true
+        ? IconButton(
             icon: const Icon(Icons.arrow_back_ios),
             onPressed: () {
               print('Back button is clicked');
             },
-          ),
-        if (title != null)
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-      ],
-    ),
+          )
+        : null,
   );
 }
