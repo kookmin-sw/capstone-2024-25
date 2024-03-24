c42522599c7342e2d913b5c64174b74fba3f41a7
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-24 11:19:14 +0900

    fix: 로그 출력하도록 변경

--- backend/src/main/java/capstone/allbom/routine/service/RoutineService.java
+++ backend/src/main/java/capstone/allbom/routine/service/RoutineService.java
@@ -4,6 +4,7 @@ import capstone.allbom.routine.domain.Routine;
 import capstone.allbom.routine.domain.RoutineRepository;
 import capstone.allbom.routine.infrastructure.api.RestTemplateRoutineRequester;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,15 +16,16 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 @EnableScheduling
+@Slf4j
 public class RoutineService {
 
     private final RoutineRepository routineRepository;
     private final RestTemplateRoutineRequester restTemplateRoutineRequester;
 
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
-//    @Scheduled(cron = "0 13 11 * * ?")
+//    @Scheduled(cron = "0 15 11 * * ?")
     public void dailyRoutineUpdate() {
-        System.out.println("실행!!!!!!!!!!!!!!!!!!!!!");
+        log.info("매일 자동으로 루틴이 업데이트된다.");
         List<Routine> all = routineRepository.findAll();
         List<String> routines = restTemplateRoutineRequester.getRandomRoutineFields();
 
