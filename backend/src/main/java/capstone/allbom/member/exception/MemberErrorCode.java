df8bc71ad98ef8e11e3baec622f77b2253e459d2
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-30 07:15:29 +0900

    feat: MemberService 추가

---
+++ backend/src/main/java/capstone/allbom/member/exception/MemberErrorCode.java
@@ -0,0 +1,16 @@
+package capstone.allbom.member.exception;
+
+import capstone.allbom.common.exception.DefaultErrorCode;
+import capstone.allbom.common.exception.ErrorCode;
+
+public enum MemberErrorCode implements ErrorCode {
+    NON_EXISTENT_MEMBER(803, "해당 회원이 존재하지 않습니다.");
+
+    private final int code;
+    private final String message;
+
+    MemberErrorCode(final int code, final String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
