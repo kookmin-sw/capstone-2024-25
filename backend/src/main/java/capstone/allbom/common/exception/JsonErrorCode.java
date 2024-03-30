3e347acf050dcfb5910d962d1e73723d8a86d051
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-30 12:24:52 +0900

    feat: Json 관련 에러 코드 추가

---
+++ backend/src/main/java/capstone/allbom/common/exception/JsonErrorCode.java
@@ -0,0 +1,19 @@
+package capstone.allbom.common.exception;
+
+public enum JsonErrorCode implements ErrorCode{
+
+    UNEXPECTED_EXCEPTION(2000, "예상치 못한 Json관련 문제가 발생했습니다.");
+    private final int code;
+    private final String message;
+
+    JsonErrorCode(final int code, final String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+
