202cdd10a0484ba1a39308254fc43ca6810a21f0
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-02 12:42:18 +0900

    feat: Refresh 토큰 리포지토리 생성

---
+++ backend/src/main/java/capstone/allbom/auth/domain/RefreshTokenRepository.java
@@ -0,0 +1,6 @@
+package capstone.allbom.auth.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+}
