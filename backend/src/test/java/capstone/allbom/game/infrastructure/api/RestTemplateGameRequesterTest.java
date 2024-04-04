ad19315c95b0079a6aa0457e3faa28d838daf2bf
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-04 21:59:34 +0900

    feat: 게임 문장 json 파일 조회하여 String 타입으로 변환하는 서비스 로직에 대한 테스트 코드 작성

--- backend/src/test/java/capstone/allbom/game/infrastructure/api/RestTemplateGameRequesterTest.java
+++ backend/src/test/java/capstone/allbom/game/infrastructure/api/RestTemplateGameRequesterTest.java
@@ -1,7 +1,21 @@
 package capstone.allbom.game.infrastructure.api;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class RestTemplateGameRequesterTest {
+
+    @Autowired
+    private RestTemplateGameRequester restTemplateGameRequester;
+
+    @Test
+    public void 문장_순서_맞추기_파일을_문자열로_변환한다() {
+        String sentenceData = restTemplateGameRequester.requestGame();
+        System.out.println("sentenceData = " + sentenceData);
+    }
 
 }
