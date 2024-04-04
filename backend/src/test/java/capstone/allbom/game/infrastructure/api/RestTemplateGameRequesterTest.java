fa2fd52f84e83901396cf8e12c3f018f8a9d001b
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-05 06:11:08 +0900

    feat: 존재하지 않는 과목명으로 문장 가져오는 경우에 대한 테스트 코드 작성

--- backend/src/test/java/capstone/allbom/game/infrastructure/api/RestTemplateGameRequesterTest.java
+++ backend/src/test/java/capstone/allbom/game/infrastructure/api/RestTemplateGameRequesterTest.java
@@ -34,4 +34,15 @@ class RestTemplateGameRequesterTest {
         assertThat(scienceSentence).isEqualTo("전자 자기 공명 이미징은 분자의 구조와 성질을 연구하는 데 사용된다.");
     }
 
+    @Test
+    public void 존재하지_않는_과목명으로_문장을_조회한다() {
+        // when
+        BadRequestException e = assertThrows(BadRequestException.class, ()
+                -> restTemplateGameRequester.getSentence("법률안", "3"));
+
+        // then
+        assertThat(e.getErrorCode()).isEqualTo(INVALID_GAME_SUBJECT_TYPE);
+        assertThat(e.getMessage()).isEqualTo("요청한 과목명이나 번호에 해당하는 문장이 존재하지 않습니다.");
+    }
+
 }
