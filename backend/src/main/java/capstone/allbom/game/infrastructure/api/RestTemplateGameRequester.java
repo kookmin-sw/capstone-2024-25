d50a4f7f0f3be6ce131406a769fdfcf0f1285906
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-04 21:50:34 +0900

    feat: AWS S3에서 게임 문장 json 파일 조회하여 String 타입으로 변환하는 로직 작성

---
+++ backend/src/main/java/capstone/allbom/game/infrastructure/api/RestTemplateGameRequester.java
@@ -0,0 +1,43 @@
+package capstone.allbom.game.infrastructure.api;
+
+import capstone.allbom.common.service.S3FileService;
+import capstone.allbom.game.domain.GameRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class RestTemplateGameRequester {
+
+    private final S3FileService s3FileService;
+    private final RestTemplate restTemplate;
+    private String GAME_REQUEST_URL;
+
+    public String requestGame() {
+        GAME_REQUEST_URL = s3FileService.getFile("gameSentence.json");
+
+        restTemplate.getMessageConverters()
+                .add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+
+        RequestEntity<Void> requestEntity = RequestEntity
+                .get(URI.create(GAME_REQUEST_URL))
+                .accept(MediaType.TEXT_PLAIN)
+                .build();
+
+        String sentenceData = restTemplate
+                .exchange(requestEntity, String.class)
+                .getBody();
+
+        log.info("sentenceData={}", sentenceData);
+        return sentenceData;
+    }
+}
---
+++ backend/src/test/java/capstone/allbom/game/infrastructure/api/RestTemplateGameRequesterTest.java
@@ -0,0 +1,7 @@
+package capstone.allbom.game.infrastructure.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestTemplateGameRequesterTest {
+
+}
