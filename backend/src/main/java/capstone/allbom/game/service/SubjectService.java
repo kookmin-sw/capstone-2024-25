8842190d26c8df9f5e7c16517fe535741db3962d
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-06 00:53:58 +0900

    feat: 다음 문제로 업데이트하기 위한 서비스 로직 작성

--- backend/src/main/java/capstone/allbom/game/service/SubjectService.java
+++ backend/src/main/java/capstone/allbom/game/service/SubjectService.java
@@ -1,5 +1,6 @@
 package capstone.allbom.game.service;
 
+import capstone.allbom.common.exception.BadRequestException;
 import capstone.allbom.common.exception.DefaultErrorCode;
 import capstone.allbom.common.exception.NotFoundException;
 import capstone.allbom.game.domain.Subject;
@@ -27,6 +28,11 @@ public class SubjectService {
 
     @Transactional(readOnly = true)
     public String getCurrSentence(SubjectType type, Integer problemNum) {
+        String subjectType = convertToRequestType(type);
+        return gameRequester.getSentence(subjectType, problemNum.toString());
+    }
+
+    private String convertToRequestType(SubjectType type) {
         String requestType = switch (type) {
             case LITERATURE -> "문학";
             case SCIENCE -> "과학";
@@ -34,7 +40,34 @@ public class SubjectService {
             case HISTORY -> "역사";
             case LEGISLATION -> "법률";
         };
-        return gameRequester.getSentence(requestType, problemNum.toString());
+        return requestType;
     }
+
+    @Transactional
+    public void updateToNextProblem(Subject subject) {
+        String subjectType = convertToRequestType(subject.getType());
+        if (subject.isCompleteExcludePassed()) { // 건너뛴 문제 제외하고 모든 문제 완료
+            if (subject.getPassedProblems().size() > 0) {
+                subject.setCurrProblem(subject.getPassedProblems().get(0));
+                subject.getPassedProblems().remove(0);
+            } else { // 모든 문제 완료
+                throw new BadRequestException(DefaultErrorCode.COMPLETE_SUBJECT_ALL_PROBLEM);
+            }
+        } else {
+            if (gameRequester.getSubjectData(subjectType).size() <= subject.getCurrProblem()) { // 현재의 문제가 마지막 문제이거나 인덱스 범위를 초과했다면
+                subject.setCompleteExcludePassed(true);
+                if (subject.getPassedProblems().size() > 0) {
+                    subject.setCurrProblem(subject.getPassedProblems().get(0));
+                    subject.getPassedProblems().remove(0);
+                } else { // 모든 문제 완료
+                    throw new BadRequestException(DefaultErrorCode.COMPLETE_SUBJECT_ALL_PROBLEM);
+                }
+            } else {
+                subject.setCurrProblem(subject.getCurrProblem() + 1);
+            }
+        }
+    }
+
+
 
 }
