aba7cee9f3b8df164d89c8c854e5e42ea9d1a48a
Vargun <heegun4690@naver.com>
2024-04-06 15:39:30 +0900

    feat: Chatbot - Input 모바일 대응 제작 중

--- frontend/project_react/src/components/Toggle/Category.js
+++ frontend/project_react/src/components/Toggle/Category.js
@@ -18,12 +18,40 @@ const ToggleStyled = styled.button`
   text-align: center;
   border-radius: 30px;
   ${({ selected, color }) => selecteds[selected]}
+  position: relative;
+`;
+// Selected 이고 values.length 가 0이 아니면 띄울 놈
+const InnerValues = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  box-sizing: border-box;
+  //width: fit-content;
+  background: url(process.env.PUBLIC_URL + '/images/Chatbot/category-bubble-small.svg')
+    no-repeat;
+  position: absolute;
+  top: 100%;
+  left: 50%;
 `;
 
-const Category = ({ text, selected, color }) => {
+const Category = ({ text, selected, color, values, onClick }) => {
   return (
-    <ToggleStyled selected={selected} color={color}>
+    <ToggleStyled selected={selected} color={color} onClick={onClick}>
       {text}
+      {selected && values.length > 0 && (
+        <InnerValues>
+          {values.map((value) => (
+            <Category
+              key={value.id}
+              text={value.title}
+              selected={value.selected}
+              color={color}
+              values={value.values}
+            />
+          ))}
+        </InnerValues>
+      )}
     </ToggleStyled>
   );
 };
--- frontend/project_react/src/pages/Chatbot.js
+++ frontend/project_react/src/pages/Chatbot.js
@@ -6,6 +6,7 @@ import ChatbotModalFirst from '../components/Modal/ChatbotFirst';
 import ChatbotModalSecond from '../components/Modal/ChatbotSecond';
 import Chat from '../components/Chatbot/Chat';
 import ChatInput from '../components/Chatbot/ChatInput';
+import Category from '../components/Toggle/Category';
 import { useState } from 'react';
 
 const ChatbotContainer = styled.div`
@@ -25,11 +26,18 @@ const ChattingWrapper = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   gap: 16px;
-  padding: 0 24px 12px 24px;
+  padding: 12px 24px;
   box-sizing: border-box;
   width: 100%;
   border: 1px solid red;
+  position: relative;
 `;
+
+const CategoryWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
 const Footer = styled.div`
   position: fixed;
   display: flex;
@@ -46,6 +54,40 @@ const Chatbot = () => {
   const [isOpenFirst, setIsOpenFirst] = useState(false);
   const [isOpenSecond, setIsOpenSecond] = useState(false);
   const [userText, setUserText] = useState('');
+  const categoryList = [
+    { id: 1, title: '날씨', selected: false, values: [] },
+    { id: 2, title: '뉴스', selected: false, values: [] },
+    {
+      id: 3,
+      title: '문화',
+      selected: false,
+      values: [
+        { id: 1, title: '교육', selected: false, values: [] },
+        { id: 2, title: '공원', selected: false, values: [] },
+        { id: 3, title: '쇼핑', selected: false, values: [] },
+      ],
+    },
+    {
+      id: 4,
+      title: '방문서비스',
+      selected: false,
+      values: [
+        { id: 1, title: '간호', selected: false, values: [] },
+        { id: 2, title: '목욕', selected: false, values: [] },
+        { id: 3, title: '요양', selected: false, values: [] },
+      ],
+    },
+  ];
+  const categoryClick = (id) => {
+    console.log('gmlgmlmglm');
+    const newCategoryList = categoryList.map((category) =>
+      category.id === id
+        ? { ...category, selected: !category.selected }
+        : category,
+    );
+    console.log(newCategoryList);
+  };
+
   const chatListDummy = [
     {
       id: 1,
@@ -62,41 +104,41 @@ const Chatbot = () => {
       text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
       type: 'System',
     },
-    {
-      id: 4,
-      text: 'nope',
-      type: 'User',
-    },
-    {
-      id: 5,
-      text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
-      type: 'System',
-    },
-    {
-      id: 6,
-      text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
-      type: 'User',
-    },
-    {
-      id: 7,
-      text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
-      type: 'System',
-    },
-    {
-      id: 8,
-      text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
-      type: 'User',
-    },
-    {
-      id: 9,
-      text: 'nope',
-      type: 'System',
-    },
-    {
-      id: 10,
-      text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
-      type: 'User',
-    },
+    // {
+    //   id: 4,
+    //   text: 'nope',
+    //   type: 'User',
+    // },
+    // {
+    //   id: 5,
+    //   text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
+    //   type: 'System',
+    // },
+    // {
+    //   id: 6,
+    //   text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
+    //   type: 'User',
+    // },
+    // {
+    //   id: 7,
+    //   text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
+    //   type: 'System',
+    // },
+    // {
+    //   id: 8,
+    //   text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
+    //   type: 'User',
+    // },
+    // {
+    //   id: 9,
+    //   text: 'nope',
+    //   type: 'System',
+    // },
+    // {
+    //   id: 10,
+    //   text: 'nopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenopenope',
+    //   type: 'User',
+    // },
   ];
 
   return (
@@ -122,10 +164,21 @@ const Chatbot = () => {
         {chatListDummy.map((chat) => (
           <Chat text={chat.text} type={chat.type} key={chat.id} />
         ))}
+        <CategoryWrapper>
+          {categoryList.map((category) => (
+            <Category
+              key={category.id}
+              text={category.title}
+              selected={category.selected}
+              color={'#FFD700'}
+              values={category.values}
+              onClick={() => categoryClick(category.id)}
+            />
+          ))}
+        </CategoryWrapper>
       </ChattingWrapper>
       <ChatInput userText={userText} setUserText={setUserText} />
       {/*<Footer>하단바 영역</Footer>*/}
-      {/*  하단바는 플러터로 구현할 예정이므로 겹치는건 문제 없을 것이라고 판단 */}
     </ChatbotContainer>
   );
 };
