98e126b1e0a8c74a6c8dd4acec7b554d1b2ff6ce
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-09 12:25:37 +0900

    feat: 게임 문장 조회하는 GET API  작성

---
+++ backend/src/main/java/capstone/allbom/game/controller/SubjectController.java
@@ -0,0 +1,36 @@
+package capstone.allbom.game.controller;
+
+import capstone.allbom.common.jwt.Auth;
+import capstone.allbom.game.domain.Subject;
+import capstone.allbom.game.domain.SubjectType;
+import capstone.allbom.game.dto.SentenceAnswerResponse;
+import capstone.allbom.game.dto.SentenceResponse;
+import capstone.allbom.game.service.SubjectService;
+import capstone.allbom.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/game")
+@Slf4j
+public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    @GetMapping
+    public ResponseEntity<SentenceResponse> getSentence(
+            @Auth final Member member,
+            @RequestParam final String type
+    ) {
+        SubjectType subjectType = SubjectType.valueOf(type.toUpperCase());
+        Subject subject = subjectService.findByMemberAndType(member, subjectType);
+        String sentence = subjectService.getCurrSentence(subjectType, subject.getCurrProblem());
+        return ResponseEntity.ok(SentenceResponse.from(subject, sentence));
+    }
+
+}
