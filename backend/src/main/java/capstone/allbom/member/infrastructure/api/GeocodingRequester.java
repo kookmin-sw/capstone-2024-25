192a5b366988a28c39dc666e8919acdb3bfa0c85
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-13 04:46:25 +0900

    feat: 위경도 변환을 위해 Naver Geocoding API 요청 보내는 로직 작성

--- backend/src/main/java/capstone/allbom/member/infrastructure/api/GeocodingRequester.java
+++ backend/src/main/java/capstone/allbom/member/infrastructure/api/GeocodingRequester.java
@@ -1,9 +1,20 @@
 package capstone.allbom.member.infrastructure.api;
 
+import capstone.allbom.member.dto.GeocodingResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -11,9 +22,36 @@ import org.springframework.web.client.RestTemplate;
 public class GeocodingRequester {
 
     private final RestTemplate restTemplate;
-    private String GEOCODING_REQUEST_URL;
+    private static final String GEOCODING_REQUEST_URL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=";
 
-    public void convertAddress() {
+    @Value("${naver.geocoding.client-id}")
+    private String clientId;
+    @Value("${naver.geocoding.client-secret}")
+    private String clientSecret;
 
+
+    public GeocodingResponse convertAddress(String address) {
+        String encodingAddress = null;
+        try {
+            encodingAddress = URLEncoder.encode(address, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String url = GEOCODING_REQUEST_URL + encodingAddress;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-NCP-APIGW-API-KEY-ID", clientId);
+        headers.set("X-NCP-APIGW-API-KEY", clientSecret);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        final ResponseEntity<String> response = restTemplate.exchange(url,
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            System.out.println("response = " + response);
+        }
+    return null;
     }
 }
