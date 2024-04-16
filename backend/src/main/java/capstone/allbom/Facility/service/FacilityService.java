ac0e4b1ba948c8fd1c4408d3176d8e16b24ac519
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-16 16:57:29 +0900

    feat : 남서쪽, 북동쪽 위경도로 지도 데이터 필터링하는 쿼리 작성

--- backend/src/main/java/capstone/allbom/Facility/service/FacilityService.java
+++ backend/src/main/java/capstone/allbom/Facility/service/FacilityService.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -24,6 +26,24 @@ public class FacilityService {
          */
         Facility savedFacility = facilityRepository.save(facility);
         return savedFacility;
+    }
+
+    public List<Facility> getFacilities(Double SWlatitude, Double SWlongitude, Double NElatitude, Double NElongitude) {
+        /**
+         * TODO
+         * 반환값과 인자 변경 필요
+
+         */
+        List<Facility> facilitiesInRectangle = facilityRepository.findFacilitiesInRectangle(SWlatitude, SWlongitude, NElatitude, NElongitude);
+        System.out.println("facilitiesInRectangle = " + facilitiesInRectangle.size());
+        for (Facility facility : facilitiesInRectangle) {
+            System.out.println("facility.getId() = " + facility.getId());
+            System.out.println("facility.getName() = " + facility.getName());
+            System.out.println("facility.getAddress() = " + facility.getAddress());
+            System.out.println("facility.getLatitude() = " + facility.getLatitude());
+            System.out.println("facility.getLongitude() = " + facility.getLongitude());
+        }
+        return facilitiesInRectangle;
     }
 
 
