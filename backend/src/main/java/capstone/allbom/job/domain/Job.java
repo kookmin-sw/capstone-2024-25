543243683f64c783c542b68868a154985a25f6c4
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-19 17:36:09 +0900

    fix: Job id 자동 생성되도록 수정

--- backend/src/main/java/capstone/allbom/job/domain/Job.java
+++ backend/src/main/java/capstone/allbom/job/domain/Job.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 public class Job {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "job_id")
     private Long id;
 
