cefdf9d16b982482d240dc9431f6a2a13c810f5c
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-19 16:10:56 +0900

    feat: Job 엔티티에 생성자 추가

--- backend/src/main/java/capstone/allbom/job/domain/Job.java
+++ backend/src/main/java/capstone/allbom/job/domain/Job.java
@@ -1,16 +1,15 @@
 package capstone.allbom.job.domain;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Entity
 @Getter @Setter
 @Slf4j
 @Builder
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Job {
 
     @Id
