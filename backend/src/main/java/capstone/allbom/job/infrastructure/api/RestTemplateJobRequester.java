d1bb25dd489e4bf3bddbc5e7003068dc785035e2
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-19 16:48:02 +0900

    feat: String 타입의 일자리 데이터 Job 객체로 변환하는 로직 작성

--- backend/src/main/java/capstone/allbom/job/infrastructure/api/RestTemplateJobRequester.java
+++ backend/src/main/java/capstone/allbom/job/infrastructure/api/RestTemplateJobRequester.java
@@ -4,6 +4,7 @@ import capstone.allbom.facility.domain.Facility;
 import capstone.allbom.facility.service.dto.FacilityRequest;
 import capstone.allbom.job.domain.Job;
 import capstone.allbom.job.dto.JobRequest;
+import capstone.allbom.job.service.JobService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,6 +27,7 @@ public class RestTemplateJobRequester {
 
     private final JobCrawlingProcessBuilder jobCrawlingProcessBuilder;
     private final RestTemplate restTemplate;
+    private final JobService jobService;
     private final String JOB_REQUEST_URL = "../data/work/workData.json";
 
     public List<Job> requestJob() {
@@ -42,7 +44,25 @@ public class RestTemplateJobRequester {
                 .exchange(requestEntity, String.class)
                 .getBody();
 
-//        log.info("sentenceData={}", sentenceData);
         return makeJobs(jobData);
+    }
+
+    public List<Job> makeJobs(String rawData) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<JobRequest> jobRequests;
+
+        try {
+            jobRequests = objectMapper.readValue(rawData, new TypeReference<List<JobRequest>>() {
+            });
+        } catch (JsonProcessingException e) {
+            log.error("객체로 변환하는 과정에서 문제가 발생했습니다.");
+            throw new RuntimeException(e);
+        }
+
+        return jobRequests.stream()
+                .map(JobRequest::toJob)
+                .peek(jobService::saveJob)
+                .toList();
     }
 }
