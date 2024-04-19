ca49261935ad4693f18d2a15391d72da8446f3f4
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-19 16:36:32 +0900

    feat: Job Request dto 설계

--- backend/src/main/java/capstone/allbom/job/dto/JobRequest.java
+++ backend/src/main/java/capstone/allbom/job/dto/JobRequest.java
@@ -1,6 +1,51 @@
 package capstone.allbom.job.dto;
 
-public record JobRequest(
+import capstone.allbom.job.domain.Job;
+import capstone.allbom.job.domain.Province;
 
+import java.time.LocalDate;
+
+public record JobRequest(
+        String worknetId,
+        String province,
+        String title,
+        LocalDate deadline,
+//        String deadline,
+        String dday,
+        String career,
+        String scholarship,
+        String address,
+        Double latitude,
+        Double longitude,
+        String pay,
+        String companyImageUrl,
+        String companyName,
+        String employmentType,
+        String workType,
+        String occupation,
+        String contents,
+        String worknetUrl
 ) {
+    public Job toJob() {
+        return Job.builder()
+                .worknetId(worknetId)
+                .province(Province.valueOf(province.toUpperCase()))
+                .title(title)
+                .deadline(deadline)
+                .dday(dday)
+                .career(career)
+                .scholarship(scholarship)
+                .address(address)
+                .latitude(latitude)
+                .longitude(longitude)
+                .pay(pay)
+                .companyImageUrl(companyImageUrl)
+                .companyName(companyName)
+                .employmentType(employmentType)
+                .workType(workType)
+                .occupation(occupation)
+                .contents(contents)
+                .worknetUrl(worknetUrl)
+                .build();
+    }
 }
