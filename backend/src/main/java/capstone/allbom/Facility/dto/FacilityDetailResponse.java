707a0c0b6a7ef5628966226e0db49e287fa57e7b
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-26 12:04:18 +0900

    feat: Facility DetailResponse dto 설계

--- backend/src/main/java/capstone/allbom/Facility/dto/FacilityDetailResponse.java
+++ backend/src/main/java/capstone/allbom/Facility/dto/FacilityDetailResponse.java
@@ -1,4 +1,10 @@
 package capstone.allbom.facility.dto;
 
-public record FacilityDetailResponse() {
+public record FacilityDetailResponse(
+        Long id,
+        String type, // HOSPITAL, PHARMACY, WELFAREHOUSE, WELFARECENTER, CARECENTER
+        String name,
+        String address,
+        String phoneNumber
+) {
 }
