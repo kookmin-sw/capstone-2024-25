bbe5aace9e29c5ebe96c4d8f8638732919a8e148
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-26 18:25:50 +0900

    feat: 시설 및 일자리 등 id로 지도 데이터 상세 조회하는 GET API 작성

--- backend/src/main/java/capstone/allbom/job/dto/JobMapDetailResponse.java
+++ backend/src/main/java/capstone/allbom/job/dto/JobMapDetailResponse.java
@@ -1,17 +1,28 @@
 package capstone.allbom.job.dto;
 
+import capstone.allbom.facility.dto.MapDetailResponse;
 import capstone.allbom.job.domain.Job;
-import capstone.allbom.medicine.domain.Medicine;
-import capstone.allbom.medicine.dto.MedicineDetailResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public record JobMapDetailResponse(
-        Long id,
-        String type, // JOB
-        String occupation,
-        String address
-) {
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@AllArgsConstructor
+public class JobMapDetailResponse extends MapDetailResponse {
+    private final Long id;
+    private final String type; // 정적으로 선언
+    private final String occupation;
+    private final String address;
+
     public static JobMapDetailResponse from(Job job) {
-        String jobType = "job";
-        return new JobMapDetailResponse(job.getId(), jobType, job.getOccupation(), job.getAddress());
+        String type = "JOB";
+        return new JobMapDetailResponse(
+                job.getId(),
+                type, // 직접 정적 필드를 사용
+                job.getOccupation(),
+                job.getAddress()
+        );
     }
 }
