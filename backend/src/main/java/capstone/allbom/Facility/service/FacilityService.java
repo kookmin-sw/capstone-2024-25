27f589189c7602a5ef6706e36f36f952d1ea27c0
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-26 17:14:03 +0900

    feat: 존재하지 않는 지도 id로 찾는 경우에 대한 예외 코드 추가

--- backend/src/main/java/capstone/allbom/Facility/service/FacilityService.java
+++ backend/src/main/java/capstone/allbom/Facility/service/FacilityService.java
@@ -1,5 +1,7 @@
 package capstone.allbom.facility.service;
 
+import capstone.allbom.common.exception.DefaultErrorCode;
+import capstone.allbom.common.exception.NotFoundException;
 import capstone.allbom.facility.domain.Facility;
 import capstone.allbom.facility.domain.FacilityRepository;
 import capstone.allbom.facility.domain.FacilityType;
@@ -22,10 +24,6 @@ public class FacilityService {
 
     @Transactional
     public Facility saveFacility(final Facility facility) {
-        /**
-         * TODO
-         * 해당 함수 유지 고민 (repository 함수와 중복)
-         */
         Facility savedFacility = facilityRepository.save(facility);
         return savedFacility;
     }
