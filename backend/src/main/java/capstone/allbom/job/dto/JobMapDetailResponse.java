a14f5d5ae63a756dbd04f26d935a0428c4206299
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-26 13:10:37 +0900

    feat: Job DetailResponse dto 설계

--- backend/src/main/java/capstone/allbom/job/dto/JobMapDetailResponse.java
+++ backend/src/main/java/capstone/allbom/job/dto/JobMapDetailResponse.java
@@ -1,4 +1,9 @@
 package capstone.allbom.job.dto;
 
-public record JobMapDetailResponse() {
+public record JobMapDetailResponse(
+        Long id,
+        String type, // JOB
+        String occupation,
+        String address
+) {
 }
