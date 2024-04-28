025d4070c7c0e00ac21075a652a33c0264402245
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-29 04:02:15 +0900

    feat: 약 시간이 유효한지 검증하는 로직 작성

--- backend/src/main/java/capstone/allbom/medicine/service/dto/MedicineRequest.java
+++ backend/src/main/java/capstone/allbom/medicine/service/dto/MedicineRequest.java
@@ -1,5 +1,7 @@
 package capstone.allbom.medicine.service.dto;
 
+import capstone.allbom.common.exception.BadRequestException;
+import capstone.allbom.common.exception.DefaultErrorCode;
 import capstone.allbom.common.util.Validator;
 import capstone.allbom.medicine.domain.Medicine;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +10,7 @@ import lombok.Getter;
 import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 //@Builder
 public record MedicineRequest(
@@ -23,9 +26,16 @@ public record MedicineRequest(
         Validator.notNull(medicineTime, "medicineTime");
         Validator.minLength(medicineName, 1, "medicineName");
         Validator.maxLength(medicineName, 20, "medicineName");
-
+        validateMedicineTime(medicineTime);
     }
 
+    private void validateMedicineTime(List<String> medicineTime) {
+        Predicate<String> isValidTime = time -> time.equals("아침") || time.equals("점심") || time.equals("저녁");
+
+        if (medicineTime.stream().anyMatch(isValidTime.negate())) {
+            throw new BadRequestException(DefaultErrorCode.INVALID_MEDICINE_TIME);
+        }
+    }
     public Medicine toDomain() {
         return new Medicine(
                 null,
