b97a7870188279f56c99d52e48c8d1b98c4e62a7
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-04-29 12:00:47 +0900

    feat: Swagger Config 파일 추가

---
+++ backend/src/main/java/capstone/allbom/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package capstone.allbom.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "Jwt",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Allbom-API-Document")
+                .description("올봄 API 문서입니다.")
+                .version("1.0.0");
+    }
+}
