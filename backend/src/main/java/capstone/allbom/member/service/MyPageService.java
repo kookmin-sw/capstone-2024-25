7905388f21ad4cf5519d43427837e45a2f31122e
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-05 12:42:04 +0900

    feat: 마이페이지 생년월일 업데이트 하는 서비스 로직 작성

--- backend/src/main/java/capstone/allbom/member/service/MyPageService.java
+++ backend/src/main/java/capstone/allbom/member/service/MyPageService.java
@@ -1,8 +1,14 @@
 package capstone.allbom.member.service;
 
+import capstone.allbom.common.exception.BadRequestException;
+import capstone.allbom.common.exception.DefaultErrorCode;
 import capstone.allbom.game.domain.GameRepository;
+import capstone.allbom.job.domain.Province;
 import capstone.allbom.medicine.domain.MedicineRepository;
+import capstone.allbom.member.domain.Gender;
+import capstone.allbom.member.domain.Member;
 import capstone.allbom.member.domain.MemberRepository;
+import capstone.allbom.member.dto.*;
 import capstone.allbom.member.infrastructure.api.GeocodingRequester;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class MyPageService {
-    private final MemberRepository memberRepository;
-    private final MedicineRepository medicineRepository;
+    private final MemberService memberService;
     private final GeocodingRequester geocodingRequester;
 
+    @Transactional
+    public void updateBirthday(final Member member, BirthdayUpdateRequest birthdayRequest) {
+        member.setBirthday(birthdayRequest.birthday());
+    }
 
 }
