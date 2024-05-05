bfd5967b50c7ba4c757b95058e8ba9364266e817
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-05 15:40:09 +0900

    hotfix: 일자리 리스트 조회 API Docs에서 sorted 필드에 @Positive 어노테이션을 붙여 발생한 400 유효성 검증 에러 해결

--- backend/src/main/java/capstone/allbom/job/controller/JobControllerDocs.java
+++ backend/src/main/java/capstone/allbom/job/controller/JobControllerDocs.java
@@ -35,7 +35,7 @@ public interface JobControllerDocs {
     ResponseEntity<List<JobListResponse>> getJobs(
             final Member member,
             @Parameter(description = "정렬 유형 (0: 가까운 순, 1: 마감일자 순)", example = "0, 1")
-            @Positive(message = "정렬 유형은 0과 1만 가능합니다.") final Integer sorted,
+            final Integer sorted,
             @Parameter(description = "페이지네이션 (page: 현재 페이지 수 - 1, size: 10으로 고정)", example = "page=0&size=10")
             Pageable pageable
     );
