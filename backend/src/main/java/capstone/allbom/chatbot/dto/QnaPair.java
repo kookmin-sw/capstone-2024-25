eee6bed4353d9dbb65c79468d4ecbbe578487947
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-07 00:39:13 +0900

    fix: QnaResponse 객체를 QnaPair 객체로 분리

---
+++ backend/src/main/java/capstone/allbom/chatbot/dto/QnaPair.java
@@ -0,0 +1,15 @@
+package capstone.allbom.chatbot.dto;
+
+import capstone.allbom.chatbot.domain.Qna;
+import capstone.allbom.member.domain.Member;
+
+public record QnaPair(
+        String question,
+        String answer
+) {
+    public static QnaPair from(Qna qna) {
+        return new QnaPair(
+                qna.getQuestion(), qna.getAnswer()
+        );
+    }
+}
--- backend/src/main/java/capstone/allbom/chatbot/dto/QnaResponse.java
+++ backend/src/main/java/capstone/allbom/chatbot/dto/QnaResponse.java
@@ -4,15 +4,16 @@ import capstone.allbom.chatbot.domain.Qna;
 import capstone.allbom.member.domain.Member;
 import capstone.allbom.member.dto.MyPageResponse;
 
+import java.util.List;
+
 // 질문-응답 쌍 조회 시 호출 (클라이언트 -> 서버)
 public record QnaResponse(
         String chatProfileImageUrl,
-        String question,
-        String answer
+        List<QnaPair> qnaPairs
 ) {
-    public static QnaResponse from(Member member, Qna qna) {
+    public static QnaResponse from(Member member, List<QnaPair> qnaPairs) {
         return new QnaResponse(
-                member.getChatProfileImageUrl(), qna.getQuestion(), qna.getAnswer()
+                member.getChatProfileImageUrl(), qnaPairs
         );
     }
 }
