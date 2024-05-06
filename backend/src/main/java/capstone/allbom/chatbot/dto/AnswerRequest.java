b2fbfd009875f480c1e0ce666178f06d6a9b69e9
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-06 21:30:28 +0900

    feat: 서버 -> AI에 질문으로 대답을 요청하는 Answer Request dto 설계

--- backend/src/main/java/capstone/allbom/chatbot/dto/AnswerRequest.java
+++ backend/src/main/java/capstone/allbom/chatbot/dto/AnswerRequest.java
@@ -1,5 +1,17 @@
 package capstone.allbom.chatbot.dto;
 
+import capstone.allbom.chatbot.domain.Qna;
+import capstone.allbom.member.domain.Member;
+
 // 응답 조회하기 위해 호출 - 2 (서버 -> AI)
-public record AnswerRequest() {
+public record AnswerRequest(
+        String isGame,
+        String question,
+        String gender
+) {
+    public static AnswerRequest from(Member member, QuestionRequest questionRequest) {
+        return new AnswerRequest(
+                questionRequest.isGame(), questionRequest.question(), member.getGender().toString()
+        );
+    }
 }
