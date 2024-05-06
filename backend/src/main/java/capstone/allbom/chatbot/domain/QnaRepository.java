f58e09e4c8c3cddc13cbc30e0efc589098ff8d28
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-06 15:15:31 +0900

    feat: 챗봇 조회 시 시간 역순으로 정렬하여 Qna 조회하는 쿼리 작성

--- backend/src/main/java/capstone/allbom/chatbot/domain/QnaRepository.java
+++ backend/src/main/java/capstone/allbom/chatbot/domain/QnaRepository.java
@@ -1,6 +1,11 @@
 package capstone.allbom.chatbot.domain;
 
+import capstone.allbom.job.domain.Job;
+import capstone.allbom.job.domain.Province;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +20,9 @@ public interface QnaRepository extends JpaRepository<Qna, Long> {
     List<Qna> findByTwentyQuestionsId(Long twentyQuestionsId);
 
     List<Qna> findAll();
+
+    List<Qna> findAllByOrderByCreatedAtDesc();
+
+    @Query("SELECT q FROM Qna q ORDER BY q.createdAt DESC")
+    List<Qna> findAllByOrderByCreatedAtPagination(Pageable pageable);
 }
