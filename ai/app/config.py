4f83edeb1183e6f1ce0e65e207f9dd6b12d9ff15
김민선 <alstjs4739@kookmin.ac.kr>
2024-05-07 19:15:42 +0900

    feat: 챗봇 데이터 기반 답변 기능 구현

--- ai/app/config.py
+++ ai/app/config.py
@@ -4,5 +4,8 @@ from dotenv import load_dotenv
 # 환경변수 파일(.env) 로드
 load_dotenv()
 
-# 설정 변수 정의
+# api_key 정의
 API_KEY = os.getenv("API_KEY")
+
+# RDS DB 연결
+DB_URI = os.getenv("DB_URI")
--- ai/app/main.py
+++ ai/app/main.py
@@ -1,13 +1,20 @@
-from config import API_KEY
+from config import API_KEY, DB_URI
 from ai.modules.daily_conversation import handle_daily_conversation
+from ai.modules.dataBased import hadle_data_based
 
 def main():
     query = input("질문을 입력해주세요: ")
     api_key = API_KEY
+    db_uri = DB_URI
+    # 함수 실행을 위한 임시 값
     gender = "MALE"
+    address = "경기도 남양주시 경춘로 1037"
 
     # 사용자의 입력을 분석하여 적절한 모듈 호출
-    response = handle_daily_conversation(api_key, query, gender)
+    if "교육" in query:
+        response = hadle_data_based(api_key, query, db_uri, address)
+    else:
+        response = handle_daily_conversation(api_key, query, gender)
 
     print(response)
 
