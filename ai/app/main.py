fcdb4af269e7fa38dd37e2c271633fdd1a1fd1da
김민선 <alstjs4739@kookmin.ac.kr>
2024-05-08 23:30:07 +0900

    feat: 챗봇 날씨 API 기반 답변 기능 구현

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,9 +1,8 @@
 from config import API_KEY, DB_URI, NEWS_API_KEY, WEATHER_API_KEY
 from ai.modules.daily_conversation import handle_daily_conversation
 from ai.modules.data_based import handle_data_based
-from ai.modules.api_based import handle_api_based
-from ai.services.api_service import get_weather_data
-
+from ai.modules.api_based import handle_news_api_based
+from ai.modules.api_based import handle_weather_api_based
 
 def main():
     query = input("질문을 입력해주세요: ")
@@ -14,18 +13,14 @@ def main():
     # 함수 실행을 위한 임시 값
     gender = "MALE"
     address = "경기도 남양주시 경춘로 1037"
-    base_date = "20240507"
-    base_time = "0800"
-    nx = "62"
-    ny = "123"
 
     # 사용자의 입력을 분석하여 적절한 모듈 호출
     if "교육" in query:
         response = handle_data_based(api_key, query, db_uri, address)
     if "뉴스" in query:
-        response = handle_api_based(api_key, news_api_key, query)
+        response = handle_news_api_based(api_key, news_api_key, query)
     if "날씨" in query:
-        response = get_weather_data(weather_api_key, base_date, base_time, nx, ny)
+        response = handle_weather_api_based(api_key, weather_api_key, gender, address)
     else:
         response = handle_daily_conversation(api_key, query, gender)
 
