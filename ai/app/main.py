6decd4be04ba267492e91188c9c8bac0006e873a
김민선 <alstjs4739@kookmin.ac.kr>
2024-05-12 22:36:55 +0900

    feat: 챗봇 질문 분류 함수 few-shot 적용

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,5 +1,8 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts.few_shot import FewShotPromptTemplate
+from langchain_core.prompts.prompt import PromptTemplate
+
 from config import API_KEY, DB_URI, NEWS_API_KEY, WEATHER_API_KEY
 from ai.modules.daily_conversation import handle_daily_conversation
 from ai.modules.data_based import handle_data_based
@@ -9,39 +12,63 @@ from ai.modules.api_based import handle_weather_api_based
 
 # 질문 분류 함수
 def classify_query(query):
+    # 예시 설정
+    examples = [
+        {"input": "산책 가고싶어.", "output": "일상대화"},
+        {"input": "산책할 곳 어디 없나?", "output": "문화"},
+        {"input": "새로운 것을 배워보고 싶어.", "output": "문화"},
+        {"input": "팔을 다쳐서 못 씻었어.", "output": "방문서비스"},
+        {"input": "정치권에 어떤 소식이 있는지 궁금해", "output": "뉴스"},
+        {"input": "스포츠 분야에 오늘 어떤 사건이 있었어?", "output": "뉴스"},
+        {"input": "장을 보러가야하는데 주변 시장 추천해줘.", "output": "문화"},
+        {"input": "밖에 나가서 걷고 싶어. 걸을 만한 곳 추천해줘.", "output": "문화"},
+    ]
+
+    # 프롬프트 템플릿 생성
+    example_prompt = PromptTemplate(
+        input_variables=["input", "output"],
+        template="{input}\ncategory: {output}"
+    )
+
+    # Few-shot 프롬프트 템플릿 생성
+    few_shot_prompt = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        input_variables=["input"],
+        suffix=f"""
+                사용자의 질문에 가장 잘 맞는 카테고리를 분류해야해. 각 카테고리에는 특정 주제가 있어.
+                '뉴스': 정치, 경제, 세계, 스포츠, IT/과학 관련 질문
+                '문화': 교육, 공원, 쇼핑 관련 질문
+                '방문서비스': 간호, 목욕, 요양 서비스 관련 질문
+                '날씨': 기상 조건 관련 질문
+                '일상대화': 그 외 일반 대화나 소셜 상호작용 관련 질문
+
+                사용자의 질문을 분석하여 가장 적절한 카테고리를 선택해야해.
+                질문의 내용과 맥락을 고려하여 가장 유사한 카테고리를 선택해줘.
+                뉴스, 문화, 방문서비스, 날씨 카테고리의 세부 내용을 참고하여 그 내용을 제공할 수 있으면 해당 카테고리로 분류해줘.
+                
+                위의 네가지 카테고리에 제공할 수 있는 내용이 없다면, 일상대화로 분류해줘.
+            
+                사용자 질문: '{query}'
+                이에 대한 답변은 category: 선택된 카테고리 형식으로 제공해줘.
+            """
+    )
+
     # openAI api key 설정
     os.environ["OPENAI_API_KEY"] = API_KEY
 
     # gpt-4 모델 설정
     model = ChatOpenAI(model_name="gpt-4")
 
-    # 모델에 입력할 질문
-    query = f"""
-                사용자의 질문에 가장 잘 맞는 카테고리를 분류해야해. 각 카테고리에는 특정 주제가 있어.
-                '뉴스' 카테고리에는 정치, 경제, 세계, 스포츠, IT/과학 등의 소주제가 포함돼.
-                '문화' 카테고리에는 교육, 공원, 쇼핑 등이 있어.
-                '방문서비스' 카테고리에는 간호, 목욕, 요양 서비스가 있어.
-                '날씨' 카테고리는 지역의 기상 조건에 관한 정보를 제공해.
-                '일상대화' 카테고리는 일반적인 대화를 포함해.
-            
-                사용자의 질문을 분석하여 가장 적절한 카테고리를 선택해야해.
-                질문의 내용과 맥락을 고려하여 가장 유사한 카테고리를 선택해줘.
-                뉴스, 문화, 방문서비스, 날씨 카테고리의 세부 내용을 참고하여 그 내용을 제공할 수 있으면 해당 카테고리로 분류해줘.
-                예를 들어, '산책 가고싶어.'라는 질문에는 공원데이터를 제공할 수 있으므로 문화 카테고리로 분류해야해.
-                그리고 '정치권에 어떤 소식이 있는지 궁금해.'라는 질문에는 정치 뉴스 데이터를 제공할 수 있으므로 뉴스 카테고리로 분류해야해.
-                위의 네가지 카테고리에 제공할 수 있는 내용이 없다면, 일상대화로 분류해줘.
-            
-                사용자 질문: '{query}'
-                이에 대한 답변은 category: 선택된 카테고리 형식으로 제공해줘.
-            """
+    # 질문 설정 및 모델 호출
+    formatted_prompt = few_shot_prompt.format()
+    result = model.invoke(formatted_prompt)
 
-    # 모델 답변 저장
-    result = model.predict(query)
-
-    # 모델 답변에서 한글 카테고리 추출
-    category = result.split(":")[1].strip()
+    # 모델 답변에서 정답 추출
+    category = result.content.split("category: ")[1].strip().split("\n")[0]
 
     return category
+
 
 def main():
     query = input("질문을 입력해주세요: ")
