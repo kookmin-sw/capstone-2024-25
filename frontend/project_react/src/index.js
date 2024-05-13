c32e3a36f7314c51216e07d3e221422daf201f27
Vargun <heegun4690@naver.com>
2024-05-13 21:13:00 +0900

    Feat : SignUp - 카카오 로딩 페이지 추가

--- frontend/project_react/src/index.js
+++ frontend/project_react/src/index.js
@@ -7,14 +7,12 @@ import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <CookiesProvider defaultSetOptions={{ path: '/' }}>
     {/* 쿠키 전역 사용을 위한 컨텍스트 제공.
     - 매번 새로운 쿠키 객체를 생성하지 않고 CookiesProvider가 제공하는 컨텍스트를 활용하여 쿠키 접근
     - 페이지 이동이나 새로고침을 해도 쿠키객체가 유지됨. */}
-    <CookiesProvider defaultSetOptions={{ path: '/' }}>
-      <App />
-    </CookiesProvider>
-  </React.StrictMode>,
+    <App />
+  </CookiesProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
