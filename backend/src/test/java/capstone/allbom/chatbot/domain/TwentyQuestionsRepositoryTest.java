cd4b20d97a1c2ba52bff4e24e4f6b1610ebb3e31
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-15 04:23:32 +0900

    feat: 회원이 현재 진행 중인 스무고개 객체를 찾는 테스트 코드 작성

--- backend/src/test/java/capstone/allbom/chatbot/domain/TwentyQuestionsRepositoryTest.java
+++ backend/src/test/java/capstone/allbom/chatbot/domain/TwentyQuestionsRepositoryTest.java
@@ -1,7 +1,59 @@
 package capstone.allbom.chatbot.domain;
 
+import capstone.allbom.member.domain.Member;
+import capstone.allbom.member.domain.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TwentyQuestionsRepositoryTest {
 
+    @Autowired private TwentyQuestionsRepository twentyQuestionsRepository;
+    @Autowired private MemberRepository memberRepository;
+
+
+    @Test
+    void 현재_진행_중인_스무고개_객체를_리턴한다() {
+        // given
+        Member member1 = Member.builder()
+                .name("memberA")
+                .build();
+        memberRepository.save(member1);
+
+        Member member2 = Member.builder()
+                .name("memberB")
+                .build();
+        memberRepository.save(member2);
+
+        TwentyQuestions twentyQuestions1 = TwentyQuestions.builder()
+                .build();
+        twentyQuestionsRepository.save(twentyQuestions1);
+
+        TwentyQuestions twentyQuestions2 = TwentyQuestions.builder()
+                .build();
+        twentyQuestionsRepository.save(twentyQuestions2);
+
+        TwentyQuestions twentyQuestions3 = TwentyQuestions.builder()
+                .build();
+        twentyQuestionsRepository.save(twentyQuestions3);
+
+        twentyQuestions1.setMember(member1);
+        twentyQuestions2.setMember(member1);
+        twentyQuestions3.setMember(member2);
+
+        // when
+        twentyQuestions3.setIsComplete(false);
+        TwentyQuestions twentyQuestions = twentyQuestionsRepository.findByMemberId(member2.getId()).get();
+
+        // then
+        Assertions.assertThat(twentyQuestions).isEqualTo(twentyQuestions3);
+    }
 }
