dd021eb7ed5df43435f97d0035f523454596d448
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-15 04:28:01 +0900

    feat: 이미 완료한 스무고개 게임은 조회되지 않는지 검증하는 테스트 코드 작성

--- backend/src/test/java/capstone/allbom/chatbot/domain/TwentyQuestionsRepositoryTest.java
+++ backend/src/test/java/capstone/allbom/chatbot/domain/TwentyQuestionsRepositoryTest.java
@@ -56,4 +56,42 @@ class TwentyQuestionsRepositoryTest {
         // then
         Assertions.assertThat(twentyQuestions).isEqualTo(twentyQuestions3);
     }
+
+    @Test
+    void 이미_완료한_스무고개_게임은_조회되지_않는다() {
+        // given
+        Member member1 = Member.builder()
+                .name("memberA")
+                .build();
+        memberRepository.save(member1);
+
+        Member member2 = Member.builder()
+                .name("memberB")
+                .build();
+        memberRepository.save(member2);
+
+        TwentyQuestions twentyQuestions1 = TwentyQuestions.builder()
+                .build();
+        twentyQuestionsRepository.save(twentyQuestions1);
+
+        TwentyQuestions twentyQuestions2 = TwentyQuestions.builder()
+                .build();
+        twentyQuestionsRepository.save(twentyQuestions2);
+
+        TwentyQuestions twentyQuestions3 = TwentyQuestions.builder()
+                .build();
+        twentyQuestionsRepository.save(twentyQuestions3);
+
+        twentyQuestions1.setMember(member1);
+        twentyQuestions2.setMember(member1);
+        twentyQuestions3.setMember(member2);
+
+        // when
+        twentyQuestions1.setIsComplete(true);
+        twentyQuestions2.setIsComplete(false);
+        TwentyQuestions twentyQuestions = twentyQuestionsRepository.findByMemberId(member1.getId()).get();
+
+        // then
+        Assertions.assertThat(twentyQuestions).isEqualTo(twentyQuestions2);
+    }
 }
