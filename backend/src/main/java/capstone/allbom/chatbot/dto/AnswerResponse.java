40ab7f5a61039b483077b5599738647a5e0d2cb3
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-15 03:57:13 +0900

    feat: Json 객체로부터 AnswerResponse dto로 변환하는 로직 작성

--- backend/src/main/java/capstone/allbom/chatbot/dto/AnswerResponse.java
+++ backend/src/main/java/capstone/allbom/chatbot/dto/AnswerResponse.java
@@ -1,8 +1,16 @@
 package capstone.allbom.chatbot.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
 // 질문에 대한 응답 - 3 (AI -> 서버)
 public record AnswerResponse(
         String type,
         String answer
 ) {
+    public static AnswerResponse fromJson(String json) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, AnswerResponse.class);
+    }
 }
