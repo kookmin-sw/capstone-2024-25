4bc07e34bc9ad6f8f31a939f6d5d90690bcdb402
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-15 18:43:42 +0900

    feat: 대화 내역 쌍에 대한 예시 스키마 작성

--- backend/src/main/java/capstone/allbom/chatbot/dto/QnaPair.java
+++ backend/src/main/java/capstone/allbom/chatbot/dto/QnaPair.java
@@ -2,10 +2,15 @@ package capstone.allbom.chatbot.dto;
 
 import capstone.allbom.chatbot.domain.Qna;
 import capstone.allbom.member.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "대화 내역 쌍")
 public record QnaPair(
+        @Schema(description = "질문", example = "비행기야?")
         String question,
+
+        @Schema(description = "답변", example = "아니요, 비행기가 아닙니다.")
         String answer
 ) {
     public static QnaPair from(Qna qna) {
