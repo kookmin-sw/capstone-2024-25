4c8eb726d97c3c3125074878b47f788a672bbd29
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-15 18:49:28 +0900

    feat: 챗봇 질문 전송 요청에 대한 예시 스키마 작성

--- backend/src/main/java/capstone/allbom/chatbot/dto/QuestionRequest.java
+++ backend/src/main/java/capstone/allbom/chatbot/dto/QuestionRequest.java
@@ -1,8 +1,15 @@
 package capstone.allbom.chatbot.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 // 질문 요청 - 1 (클라이언트 -> 서버)
+@Schema(description = "챗봇 질문 전송 요청")
 public record QuestionRequest(
+
+        @Schema(description = "스무고개 게임인지 여부", example = "false")
         Boolean isGame,
+
+        @Schema(description = "질문", example = "비행기야?")
         String question
 ) {
 }
