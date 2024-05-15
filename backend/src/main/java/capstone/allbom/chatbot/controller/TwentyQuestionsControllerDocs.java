9903d494dcc54b1b1dcbd2ecf03c4e209ea489ec
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-05-15 18:26:41 +0900

    feat: 스무고개 채팅 내역 조회하는 API description 작성

--- backend/src/main/java/capstone/allbom/chatbot/controller/TwentyQuestionsControllerDocs.java
+++ backend/src/main/java/capstone/allbom/chatbot/controller/TwentyQuestionsControllerDocs.java
@@ -1,4 +1,37 @@
 package capstone.allbom.chatbot.controller;
 
+import capstone.allbom.chatbot.dto.QuestionRequest;
+import capstone.allbom.chatbot.dto.twentyQuestions.TwentyAnswerResponse;
+import capstone.allbom.chatbot.dto.twentyQuestions.TwentyQnaResponse;
+import capstone.allbom.common.exception.ExceptionResponse;
+import capstone.allbom.common.jwt.Auth;
+import capstone.allbom.member.domain.Member;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "스무고개", description = "스무고개 API")
 public interface TwentyQuestionsControllerDocs {
+
+    @Operation(summary = "스무고개 채팅 내역 조회하기", description = "스무고개 채팅 내역을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "스무고개 채팅 내역 조회 성공"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "챗봇 프로필 업데이트가 아직 이루어지지 않은 경우",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+            )
+    })
+    ResponseEntity<TwentyQnaResponse> getQnas(
+            final Member member
+    );
+
+
+
+
 }
