d2c258bf05fa6e101d6446c489c81bbb804f899f
hammsik <asbbrp@gmail.com>
2024-05-18 20:10:15 +0900

    feat: 상단바 투명 아이콘 검정

--- frontend/project_flutter/lib/main.dart
+++ frontend/project_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:project_flutter/allbome.dart';
 import 'package:flutter/foundation.dart';
@@ -6,12 +7,16 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Permission.camera.request();
   await Permission.microphone.request();
   await Permission.location.request();
 
   if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
     await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
   }
-  runApp(const AllBome());
+  // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+  //   statusBarColor: Color.fromARGB(0, 255, 255, 255),
+  //   systemNavigationBarIconBrightness: Brightness.dark,
+  // ));
+  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+  runApp(const MaterialApp(home: AllBome()));
 }
--- frontend/project_flutter/lib/screen/layout_nav5.dart
+++ frontend/project_flutter/lib/screen/layout_nav5.dart
@@ -27,10 +27,17 @@ class _LayoutNav5State extends State<LayoutNav5> {
   String url = "";
   double progress = 0;
   final urlController = TextEditingController();
+  bool _hasCallSupport = false;
+  Future<void>? _launched;
 
   @override
   void initState() {
     super.initState();
+    canLaunchUrl(Uri(scheme: 'tel', path: '123')).then((bool result) {
+      setState(() {
+        _hasCallSupport = result;
+      });
+    });
 
     pullToRefreshController = kIsWeb
         ? null
@@ -48,105 +55,129 @@ class _LayoutNav5State extends State<LayoutNav5> {
               }
             },
           );
+    canLaunchUrl(Uri(scheme: 'tel', path: '123')).then((bool result) {
+      setState(() {
+        _hasCallSupport = result;
+      });
+    });
+  }
+
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        InAppWebView(
-          key: webViewKey,
-          initialUrlRequest: URLRequest(
-              url: WebUri("https://allbome-for-vercel.vercel.app/map")),
-              // url: WebUri("http://192.168.141.255:3000/map")),
-          initialSettings: settings,
-          pullToRefreshController: pullToRefreshController,
-          onWebViewCreated: (controller) {
-            webViewController = controller;
-            
-            // controller.addJavaScriptHandler(
-            //     handlerName: 'handlerFoo',
-            //     callback: (args) {
-            //       // return data to the JavaScript side!
-            //       return {'bar': 'bar_value', 'baz': 'baz_value'};
-            //     });
+    return Scaffold(
+      body: Stack(
+        children: [
+          InAppWebView(
+            key: webViewKey,
+            initialUrlRequest: URLRequest(
+                url: WebUri("https://allbome-for-vercel.vercel.app/map")),
+            initialSettings: settings,
+            pullToRefreshController: pullToRefreshController,
+            onWebViewCreated: (controller) {
+              webViewController = controller;
 
-            controller.addJavaScriptHandler(
-                handlerName: 'test',
-                callback: (args) {
-                  print(args);
-                  print('으갸갹 도착');
-                  // it will print: [1, true, [bar, 5], {foo: baz}, {bar: bar_value, baz: baz_value}]
-                });
-          },
-          onLoadStart: (controller, url) {
-            setState(() {
-              this.url = url.toString();
-              urlController.text = this.url;
-            });
-          },
-          onPermissionRequest: (controller, request) async {
-            return PermissionResponse(
-                resources: request.resources,
-                action: PermissionResponseAction.GRANT);
-          },
-          shouldOverrideUrlLoading: (controller, navigationAction) async {
-            var uri = navigationAction.request.url!;
+              // controller.addJavaScriptHandler(
+              //     handlerName: 'handlerFoo',
+              //     callback: (args) {
+              //       // return data to the JavaScript side!
+              //       return {'bar': 'bar_value', 'baz': 'baz_value'};
+              //     });
 
-            if (![
-              "http",
-              "https",
-              "file",
-              "chrome",
-              "data",
-              "javascript",
-              "about"
-            ].contains(uri.scheme)) {
-              if (await canLaunchUrl(uri)) {
-                // Launch the App
-                await launchUrl(
-                  uri,
-                );
-                // and cancel the request
-                return NavigationActionPolicy.CANCEL;
+              controller.addJavaScriptHandler(
+                  handlerName: 'test',
+                  callback: (args) {
+                    print(args[0]);
+                    print('으갸갹 도착');
+                    if (_hasCallSupport) {
+                      print('전화걸기를 지원합니다.');
+                      setState(() {
+                        _launched = _makePhoneCall(args[0]);
+                      });
+                    } else {
+                      print('전화걸기를 지원하지 않습니다.');
+                    }
+                    // it will print: [1, true, [bar, 5], {foo: baz}, {bar: bar_value, baz: baz_value}]
+                  });
+            },
+            onLoadStart: (controller, url) {
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onPermissionRequest: (controller, request) async {
+              return PermissionResponse(
+                  resources: request.resources,
+                  action: PermissionResponseAction.GRANT);
+            },
+            shouldOverrideUrlLoading: (controller, navigationAction) async {
+              var uri = navigationAction.request.url!;
+
+              if (![
+                "http",
+                "https",
+                "file",
+                "chrome",
+                "data",
+                "javascript",
+                "about"
+              ].contains(uri.scheme)) {
+                if (await canLaunchUrl(uri)) {
+                  // Launch the App
+                  await launchUrl(
+                    uri,
+                  );
+                  // and cancel the request
+                  return NavigationActionPolicy.CANCEL;
+                }
               }
-            }
 
-            return NavigationActionPolicy.ALLOW;
-          },
-          onLoadStop: (controller, url) async {
-            pullToRefreshController?.endRefreshing();
-            setState(() {
-              this.url = url.toString();
-              urlController.text = this.url;
-            });
-          },
-          onReceivedError: (controller, request, error) {
-            pullToRefreshController?.endRefreshing();
-          },
-          onProgressChanged: (controller, progress) {
-            if (progress == 100) {
+              return NavigationActionPolicy.ALLOW;
+            },
+            onLoadStop: (controller, url) async {
               pullToRefreshController?.endRefreshing();
-            }
-            setState(() {
-              this.progress = progress / 100;
-              urlController.text = url;
-            });
-          },
-          onUpdateVisitedHistory: (controller, url, androidIsReload) {
-            setState(() {
-              this.url = url.toString();
-              urlController.text = this.url;
-            });
-          },
-          onConsoleMessage: (controller, consoleMessage) {
-            if (kDebugMode) {
-              print('리액트로부터.. $consoleMessage');
-            }
-          },
-        ),
-        progress < 1.0 ? LinearProgressIndicator(value: progress) : Container(),
-      ],
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onReceivedError: (controller, request, error) {
+              pullToRefreshController?.endRefreshing();
+            },
+            onProgressChanged: (controller, progress) {
+              if (progress == 100) {
+                pullToRefreshController?.endRefreshing();
+              }
+              setState(() {
+                this.progress = progress / 100;
+                urlController.text = url;
+              });
+            },
+            onUpdateVisitedHistory: (controller, url, androidIsReload) {
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onConsoleMessage: (controller, consoleMessage) {
+              if (kDebugMode) {
+                print('리액트로부터.. $consoleMessage');
+              }
+            },
+          ),
+          progress < 1.0
+              ? LinearProgressIndicator(value: progress)
+              : Container(),
+        ],
+      ),
     );
   }
 }
