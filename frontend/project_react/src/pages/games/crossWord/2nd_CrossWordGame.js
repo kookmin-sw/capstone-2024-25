b643d4c2983d7865017c448437e177986839951e
hammsik <asbbrp@gmail.com>
2024-05-18 20:28:39 +0900

    style: 심자말풀이 끝나고 완료

--- frontend/project_react/src/pages/games/crossWord/2nd_CrossWordGame.js
+++ frontend/project_react/src/pages/games/crossWord/2nd_CrossWordGame.js
@@ -2,14 +2,14 @@ import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import TitleHeader from '../../../components/Header/TitleHeader';
-import BottomButton from '../../../components/Game/bottomButton';
-import { motion, transform } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { crossWordData } from './crossWordData';
-import { set } from 'date-fns';
 import Layout from '../../../layouts/Layout';
+import Swal from 'sweetalert2';
 
 export default function CrossWordGame() {
   const [crosswordArr, setCrosswordArr] = useState([]);
+  const navigate = useNavigate();
 
   const parentRef = useRef(null);
   const [size, setSize] = useState(0);
@@ -95,154 +95,184 @@ export default function CrossWordGame() {
     return () => window.removeEventListener('resize', updateSize);
   }, []);
 
-  return (
-  <Layout>
-    <Frame>
-      <TitleHeader showBackButton={true} title={'십자말풀이'}></TitleHeader>
-      <SquareWrapper ref={parentRef}>
-        <CrosswordDiv
-          size={size}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.2 }}
-        >
-          {crosswordArr.map((row, rowIndex) => (
-            <div
-              key={rowIndex}
-              style={{ display: 'flex', width: '100%', height: '14.2857%' }}
-            >
-              {row.map((item, colIndex) => (
-                <motion.button
-                  disabled={item === '' ? true : false}
-                  key={colIndex}
-                  style={{
-                    boxSizing: 'border-box',
-                    width: '14.2857%',
-                    height: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 4,
-                    fontSize: 22,
-                    fontWeight: '600',
-                    backgroundColor: item === '' ? '#8D8D8D' : 'white',
-                    border:
-                      selectedWordRadix[0] === rowIndex &&
-                      selectedWordRadix[1] === colIndex
-                        ? item[1] === '' ? '4px solid red' : '4px solid green'
-                        : '1px solid black',
-                  }}
-                  onClick={() => {
-                    setWordSegments(getWordSegments(item[0]));
-                    const horizontalExplanation =
-                      crossWordData[0].horizontal.find(
-                        (quiz) =>
-                          quiz.row === rowIndex &&
-                          colIndex >= quiz.col &&
-                          colIndex < quiz.col + quiz.word.length,
-                      );
-                    const verticalExplanation = crossWordData[0].vertical.find(
-                      (quiz) =>
-                        quiz.col === colIndex &&
-                        rowIndex >= quiz.row &&
-                        rowIndex < quiz.row + quiz.word.length,
-                    );
-                    setSelectedWordRadix([rowIndex, colIndex]);
+  useEffect(() => {
+    crosswordArr.forEach((row, rowIndex) => {
+      row.forEach((item, colIndex) => {
+        console.log('ee',item[1])
+        if (!item[1] || item[1] === '') {
+          return;
+        }
+      });
+    });
 
-                    if (horizontalExplanation) {
-                      setSelectedHorizontalExplanation(
-                        horizontalExplanation.explanation,
-                      );
-                      console.log(horizontalExplanation.explanation);
-                    } else {
-                      setSelectedHorizontalExplanation(null);
-                    }
-                    if (verticalExplanation) {
-                      setSelectedVerticalExplanation(
-                        verticalExplanation.explanation,
-                      );
-                      console.log(verticalExplanation.explanation);
-                    } else {
-                      setSelectedVerticalExplanation(null);
-                    }
-                  }}
-                >
-                  {item[0] === item[1] ? item[0] : ''}
-                </motion.button>
-              ))}
-            </div>
-          ))}
-        </CrosswordDiv>
-        <ExplanationDiv
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.2 }}
-        >
-          {selectedHorizontalExplanation && (
-            <p style={{ margin: '0px', wordBreak: 'break-all' }}>
-              <span style={{ fontWeight: 'bold' }}>가로: </span>
-              {selectedHorizontalExplanation
-                ? selectedHorizontalExplanation
-                : ''}
-            </p>
-          )}
-          {selectedVerticalExplanation && (
-            <p style={{ margin: '0px', wordBreak: 'break-all' }}>
-              <span style={{ fontWeight: 'bold' }}>세로: </span>
-              {selectedVerticalExplanation ? selectedVerticalExplanation : ''}
-            </p>
-          )}
-        </ExplanationDiv>
-        <WordSegDiv>
-          {wordSegments.map((word, index) => (
-            <motion.button
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.3 + index * 0.02 }}
-              onClick={() => {
-                setWordSegments((prev) => {
-                  return prev.map((item, i) => i === index ? '' : item);
-                });
-                if (
-                  crosswordArr[selectedWordRadix[0]][
-                    selectedWordRadix[1]
-                  ][0] === word
-                ) {
-                  setCrosswordArr(
-                    crosswordArr.map((row, rowIndex) => {
-                      return row.map((item, colIndex) => {
-                        if (
-                          selectedWordRadix[0] === rowIndex &&
-                          selectedWordRadix[1] === colIndex
-                        ) {
-                          return [word, word];
-                        }
-                        return item;
-                      });
-                    }),
-                  );
-                }
-              }}
-              key={index}
-              style={{
-                width: 'calc(15% - 10px)',
-                height: 40,
-                borderRadius: 4,
-                border: '0px solid black',
-                backgroundColor: '#379237',
-                fontSize: 16,
-                fontWeight: '600',
-                color: 'white',
-                visibility: word === '' ? 'hidden' : 'visible',
-              }}
-            >
-              {word}
-            </motion.button>
-          ))}
-        </WordSegDiv>
-      </SquareWrapper>
-    </Frame>
-  </Layout>
+    // 모든 단어를 맞추면 완료 메시지를 띄웁니다.
+    Swal.fire({
+      icon: 'success',
+      title: '정답입니다!',
+      showDenyButton: true,
+      denyButtonText: '그만하기',
+      confirmButtonText: '다음 문제',
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log('다음 문제로 이동');
+      } else if (result.isDenied) {
+        navigate(-1, { replace: true });
+      }
+    });
+  }, [crosswordArr]);
+
+  return (
+    <Layout>
+      <Frame>
+        <TitleHeader showBackButton={true} title={'십자말풀이'}></TitleHeader>
+        <SquareWrapper ref={parentRef}>
+          <CrosswordDiv
+            size={size}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.2 }}
+          >
+            {crosswordArr.map((row, rowIndex) => (
+              <div
+                key={rowIndex}
+                style={{ display: 'flex', width: '100%', height: '14.2857%' }}
+              >
+                {row.map((item, colIndex) => (
+                  <motion.button
+                    disabled={item === '' ? true : false}
+                    key={colIndex}
+                    style={{
+                      boxSizing: 'border-box',
+                      width: '14.2857%',
+                      height: '100%',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      borderRadius: 4,
+                      fontSize: 22,
+                      fontWeight: '600',
+                      backgroundColor: item === '' ? '#8D8D8D' : 'white',
+                      border:
+                        selectedWordRadix[0] === rowIndex &&
+                        selectedWordRadix[1] === colIndex
+                          ? item[1] === ''
+                            ? '4px solid red'
+                            : '4px solid green'
+                          : '1px solid black',
+                    }}
+                    onClick={() => {
+                      setWordSegments(getWordSegments(item[0]));
+                      const horizontalExplanation =
+                        crossWordData[0].horizontal.find(
+                          (quiz) =>
+                            quiz.row === rowIndex &&
+                            colIndex >= quiz.col &&
+                            colIndex < quiz.col + quiz.word.length,
+                        );
+                      const verticalExplanation =
+                        crossWordData[0].vertical.find(
+                          (quiz) =>
+                            quiz.col === colIndex &&
+                            rowIndex >= quiz.row &&
+                            rowIndex < quiz.row + quiz.word.length,
+                        );
+                      setSelectedWordRadix([rowIndex, colIndex]);
+
+                      if (horizontalExplanation) {
+                        setSelectedHorizontalExplanation(
+                          horizontalExplanation.explanation,
+                        );
+                        console.log(horizontalExplanation.explanation);
+                      } else {
+                        setSelectedHorizontalExplanation(null);
+                      }
+                      if (verticalExplanation) {
+                        setSelectedVerticalExplanation(
+                          verticalExplanation.explanation,
+                        );
+                        console.log(verticalExplanation.explanation);
+                      } else {
+                        setSelectedVerticalExplanation(null);
+                      }
+                    }}
+                  >
+                    {item[0] === item[1] ? item[0] : ''}
+                  </motion.button>
+                ))}
+              </div>
+            ))}
+          </CrosswordDiv>
+          <ExplanationDiv
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.2 }}
+          >
+            {selectedHorizontalExplanation && (
+              <p style={{ margin: '0px', wordBreak: 'break-all' }}>
+                <span style={{ fontWeight: 'bold' }}>가로: </span>
+                {selectedHorizontalExplanation
+                  ? selectedHorizontalExplanation
+                  : ''}
+              </p>
+            )}
+            {selectedVerticalExplanation && (
+              <p style={{ margin: '0px', wordBreak: 'break-all' }}>
+                <span style={{ fontWeight: 'bold' }}>세로: </span>
+                {selectedVerticalExplanation ? selectedVerticalExplanation : ''}
+              </p>
+            )}
+          </ExplanationDiv>
+          <WordSegDiv>
+            {wordSegments.map((word, index) => (
+              <motion.button
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.3 + index * 0.02 }}
+                onClick={() => {
+                  setWordSegments((prev) => {
+                    return prev.map((item, i) => (i === index ? '' : item));
+                  });
+                  if (
+                    crosswordArr[selectedWordRadix[0]][
+                      selectedWordRadix[1]
+                    ][0] === word
+                  ) {
+                    setCrosswordArr(
+                      crosswordArr.map((row, rowIndex) => {
+                        return row.map((item, colIndex) => {
+                          if (
+                            selectedWordRadix[0] === rowIndex &&
+                            selectedWordRadix[1] === colIndex
+                          ) {
+                            return [word, word];
+                          }
+                          return item;
+                        });
+                      }),
+                    );
+                  }
+                }}
+                key={index}
+                style={{
+                  width: 'calc(15% - 10px)',
+                  height: 40,
+                  borderRadius: 4,
+                  border: '0px solid black',
+                  backgroundColor: '#379237',
+                  fontSize: 16,
+                  fontWeight: '600',
+                  color: 'white',
+                  visibility: word === '' ? 'hidden' : 'visible',
+                }}
+              >
+                {word}
+              </motion.button>
+            ))}
+          </WordSegDiv>
+        </SquareWrapper>
+      </Frame>
+    </Layout>
   );
 }
 
